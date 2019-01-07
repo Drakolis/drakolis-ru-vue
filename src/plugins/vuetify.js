c8a0e9156faf25fa52ce9cc675375d3721079060
Mika Drakolis <drakolis@drakolis.ru>
2019-01-08 01:56:39 +0300

    Color reworks

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,7 +4,7 @@ import "vuetify/dist/vuetify.min.css";
 
 Vue.use(Vuetify, {
   theme: {
-    primary: "#00A8AB",
+    primary: "#c9361d",
     secondary: "#424242",
     accent: "#82B1FF",
     error: "#FF5252",
