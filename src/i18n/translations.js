4a35b1cfbd36f063198e3b5442339de5586a4597
Mika Drakolis <dragon.fox.66@yandex.ru>
2019-01-10 17:34:00 +0300

    Fix translations

--- src/i18n/translations.js
+++ src/i18n/translations.js
@@ -1,87 +1,40 @@
 export default {
   en: {
     global: {
-      app: "Eublepharis",
-      motto: "Be more than you are",
       ok: "Ok",
       cancel: "Cancel",
       close: "Close",
       save: "Save",
       connecting: "Connecting to server...",
       reconnecting: "Connection lost. Reconnecting...",
-      profile: "Profile"
-    },
-    sidebar: {
-      collapse: "Collapse",
-      home: "Home",
-      characters: "Characters"
-    },
-    account: {
-      signin: "Sign in",
-      signout: "Sign out",
-      signup: "Sign up",
-      email: "E-Mail",
+      auth: {
+        signIn: "Sign in",
+        signOut: "Sign out",
+        signUp: "Sign up",
+
+        login: "@:global.login/@:global.email",
+        password: "@:global.password"
+      },
       userName: "User name",
+      email: "E-Mail",
       login: "Login",
-      phone: "Phone",
-      password: "Password",
-      multilogin: "@:account.login/@:account.email"
+
+      password: "Password"
+    },
+    error: {
+      development: {
+        title: "Wyverns at work!",
+        info: "This feature is under development"
+      },
+      restricted: {
+        title: "Dragons only!",
+        info: "Access is restricted only for worthy ones!"
+      }
     },
     validators: {
       required: "Required",
-      email: "Incorrect @:account.email",
+      email: "Incorrect @:global.email",
       alphanumeric: ""
-    },
-    roomsList: {
-      private: "Private chats",
-      rooms: "Rooms"
-    },
-    messageEditor: {
-      placeholder: "Enter your message...",
-      send: "Send message",
-      commands: "Chat commands"
-    }
-  },
-  ru: {
-    global: {
-      app: "Eublepharis",
-      motto: "Будь большим чем ты есть",
-      ok: "Ок",
-      cancel: "Отмена",
-      save: "Сохранить",
-      close: "Закрыть",
-      connecting: "Подключение к серверу...",
-      reconnecting: "Соединение потеряно. Переподключение...",
-      profile: "Профиль"
-    },
-    sidebar: {
-      collapse: "Свернуть",
-      home: "Главная",
-      characters: "Персонажи"
-    },
-    account: {
-      signin: "Войти",
-      signout: "Выйти",
-      signup: "Регистрация",
-      email: "E-Mail",
-      userName: "Имя пользователя",
-      login: "Логин",
-      password: "Пароль",
-      multilogin: "@:account.login/@:account.email"
-    },
-    validators: {
-      required: "Обязательно",
-      email: "Неверный @:account.email",
-      alphanumeric: ""
-    },
-    roomsList: {
-      private: "Приватные чаты",
-      rooms: "Комнаты"
-    },
-    messageEditor: {
-      placeholder: "Введите текст сообщения...",
-      send: "Отправить",
-      commands: "Команды чата"
     }
   }
 };
