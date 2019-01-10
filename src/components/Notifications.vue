2e320ef682e78cf9584be8e703289db849b50317
Mika Drakolis <dragon.fox.66@yandex.ru>
2019-01-10 17:33:17 +0300

    Notifications

---
+++ src/components/Notifications.vue
@@ -0,0 +1,25 @@
+<template>
+    <vue-snotify></vue-snotify>
+</template>
+
+<script>
+import Vue from "vue";
+import Snotify, { SnotifyPosition } from "vue-snotify";
+import "vue-snotify/styles/simple.css";
+
+const options = {
+  toast: {
+    position: SnotifyPosition.leftTop
+  }
+};
+
+Vue.use(Snotify, options);
+
+export default {};
+</script>
+
+<style>
+.snotifyToast__title {
+  font-size: 26px;
+}
+</style>
