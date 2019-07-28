f63f42d4a44446dd11704ec985c7e97feca64b49
Mika Drakolis <drakolis@drakolis.ru>
2019-07-28 17:53:22 +0300

    Fix error page

--- src/views/Error404.vue
+++ src/views/Error404.vue
@@ -2,7 +2,7 @@
   <v-container>
     <v-layout align-center justify-center row fill-height>
       <v-flex>
-        <div class="text-xs-center">
+        <div class="text-center">
           <p class="display-4 my-4">404</p>
           <small class="display-2 font-weight-light">What? Where are we?</small>
         </div>
